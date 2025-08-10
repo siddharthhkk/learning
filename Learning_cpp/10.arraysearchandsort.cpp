0705ab8f00cd97d819e06e62ca7ec57fcea25193
siddharthhkk <siddh2913@gmail.com>
2025-08-10 13:47:54 +0530

    ide change commit

--- Learning_cpp/10.arraysearchandsort.cpp
+++ Learning_cpp/10.arraysearchandsort.cpp
@@ -1,26 +1,52 @@
 #include <iostream>
 using namespace std;
 
-int search(int age[], int size, int myage);
+int search(int age[], int size, int myage1);
+void bubblesort(int array[], int size);
 
 int main() {
     int age[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
-    int myage;
+    int myage1;
     int size = sizeof(age) / sizeof(int);
     cout << "Enter your age: ";
-    cin >> myage;
-    int respond = search(age, size, myage);
+    cin >> myage1;
+    int respond = search(age, size, myage1);
     if(respond != -1)
         cout << "Your age is in " << respond << "'th index \n";
     else
         cout << "Your age is not in the array\n";
 
+    //bubble sort
+    int array[] = {64, 34, 25, 12, 22, 11, 90, 5, 77, 30};
+    int size1 = sizeof(array) / sizeof(array[0]);
+    cout << "{ ";
+    for (int i = 0; i < size1; i++)
+        cout << array[i] << ", ";
+    cout << "}";
+    bubblesort(array, size1);
+    cout << "\nSorted array: { ";
+    for (int i = 0; i < size1; i++)
+        cout << array[i] << ", ";
+    cout << "}\n";
 }
 
-int search(int age[], int size, int myage) {
+int search(int age[], int size, int myage1) {
     for (int i = 0; i < size; i++) {
-        if ( age[i] == myage)
+        if ( age[i] == myage1)
             return i;
     }
     return -1;  
+}
+
+void bubblesort(int array[], int size) {
+    int temp;
+    for (int j = 0; j < size - 1; j++) {
+        for(int i = 0; i < size - 1 - j; i++) { // -j is to avoid checking already sorted elements.
+            if (array[i] > array[i + 1]) {      // as the inside loop will send the largest number to the end we do not need to check it again.
+                temp = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = temp;
+            }
+        }
+    }
 }
