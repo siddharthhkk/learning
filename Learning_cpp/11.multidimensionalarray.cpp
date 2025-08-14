4e6bc34fc7ba708fcb160d3c130effcc9ea10aec
siddharthhkk <siddh2913@gmail.com>
2025-08-15 00:29:16 +0530

    goated commit

--- Learning_cpp/11.multidimensionalarray.cpp
+++ Learning_cpp/11.multidimensionalarray.cpp
@@ -8,14 +8,30 @@ int main() {
     std::cin >> n;
     std::cin.ignore(); // Clear the newline character left in the input buffer getline reads \n of cin, or you can just make std::getline (std::cin, n);
     std::string arr[n];
+    std::string temp[n]; // temporary stores input if user enters a valid car name we will pass it to the arr,
+                         // but if user enters "q" we will stop asking for further input.
     for (int i = 0; i < n; i++){
-        std::cout << "Enter car name " << i + 1 << ": ";
-        std::getline(std::cin, arr[i]);
-        if (arr[i] == "q") {
-            break; // when user inputs "q", the loop will break and stop asking for further input. but q will be stored in array.
+        std::cout << "Enter car name or q to quit " << i + 1 << ": ";
+        std::getline(std::cin, temp[i]);
+        if (temp[i] == "q") {
+            break;      // when user inputs "q", the loop will break and stop asking for further input. but q will also be stored in arr.
+        }
+        else {
+            arr[i] = temp[i]; // if user enters a valid car name we will pass it to the arr.
         }
     }
-    for (int i = 0; i < n; i++){
+    for (int i = 0; i != arr[i].empty(); i++){    // we will check arr[] for empty space so that we don't print the extra empty space
         std::cout << arr[i] << "\n";
     }
+    // We'll learn about dynamic arrays and vectors in future, so that we dn't need to ask user for array size or hardcode it.
+
+    //2d array -> array[rows][columns] you can just write columns, rows will be variable.
+    std::string array[][3] = {{"Sexy", "Main", "Hehe"}, {"goated", "maingoated", "secondgoated"}, {"thirdrow", "secondcolumn", "third"}};
+    for (int i = 0; i < 3; i++) { // we want to print this 3X3 2d array as a matrix.
+        for (int j = 0; j < 3; j++) {
+            std::cout << array[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+    return 0;
 }
